6ef0dfc28569c8c9fa30f2627ffb1089ba1f85ca
Joseph Bravo <jorbravoa@gmail.com>
2022-06-23 18:39:19 -0500

    ➕ Add handle exception for verify token route

--- utils/verify_token.py
+++ utils/verify_token.py
@@ -1,7 +1,7 @@
 from fastapi import Request
 from utils.jwt_token import validate_token
 from fastapi.routing import APIRoute
-
+from fastapi.responses import JSONResponse
 
 class VerifyTokenRoute(APIRoute):
     
@@ -9,8 +9,10 @@ class VerifyTokenRoute(APIRoute):
         original_route = super().get_route_handler()
         
         async def verify_token_middleware(request:Request):
-            token = request.headers["Authorization"].split(" ")[1]
-            
+            try:
+                token = request.headers["Authorization"].split(" ")[1]
+            except KeyError: 
+                return JSONResponse(content={"message": "authorization token not found"}, status_code=404)
             validation = validate_token(token, output=False)
             if validation == None:
                 return await original_route(request)
