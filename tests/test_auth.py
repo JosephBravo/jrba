a680a21393dd944eec46c00253615bc35bea9ef6
Joseph Bravo <jorbravoa@gmail.com>
2022-06-24 19:27:38 -0500

    [REVIEW] Pylint in tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -12,82 +12,82 @@ client.base_url = os.environ.get("URL_LOCAL")
 
 
 class AuthTestHelper:
+	"""
+	Auth Helper tests
+	"""
+
+	def get_token():
 		"""
-        Auth Helper tests
-        """
-
-		def get_token():
-			"""
-			Get token auth test helper
-			"""
-
-			response = client.post(
-					"/api/login",
-					json={
-						"user": "jbravonew12@example.com",
-						"password": "somepass"
-					},
-			)
-			token = jsonable_encoder(response.content).replace('"', "")
-			return token
-
-		def authenticate_user(user: str):
-			"""
-			Authenticate user test helper
-			"""
-			response = client.post(
-					"/api/login",
-					json={
-						"user": user,
-						"password": "somepass"
-					},
-			)
-			token = jsonable_encoder(response.content).replace('"', "")
-			return token
-
-def test_sing_up():
+		Get token auth test helper
 		"""
-        Test sing up
-        """
-
-		response = client.post(
-				"/api/sign_up",
-				json={
-					"user": os.environ.get("EMAIL_TEST"),
-					"password": "somepass",
-					"name": fake.name(),
-					"surname": fake.last_name(),
-					"second_surname": fake.last_name(),
-					"college": fake.company()
-				},
-		)
-		assert response.status_code == 201
-
-def test_loguin():
-		"""
-        Test loguin
-        """
 
 		response = client.post(
 				"/api/login",
 				json={
-					"user": os.environ.get("EMAIL_TEST"),
+					"user": "jbravonew12@example.com",
 					"password": "somepass"
 				},
 		)
-		assert response.status_code == 200
+		token = jsonable_encoder(response.content).replace('"', "")
+		return token
 
-def test_verify_token():
+	def authenticate_user(user: str):
 		"""
-        Test verify Token
-        """
-
+		Authenticate user test helper
+		"""
 		response = client.post(
-		"/api/verify/token",
-		headers={"Authorization": f"Bearer {AuthTestHelper.get_token()}"},
-		json={
+				"/api/login",
+				json={
+					"user": user,
+					"password": "somepass"
+				},
+		)
+		token = jsonable_encoder(response.content).replace('"', "")
+		return token
+
+def test_sing_up():
+	"""
+	Test sing up
+	"""
+
+	response = client.post(
+			"/api/sign_up",
+			json={
+				"user": os.environ.get("EMAIL_TEST"),
+				"password": "somepass",
+				"name": fake.name(),
+				"surname": fake.last_name(),
+				"second_surname": fake.last_name(),
+				"college": fake.company()
+			},
+	)
+	assert response.status_code == 201
+
+def test_loguin():
+	"""
+	Test loguin
+	"""
+
+	response = client.post(
+			"/api/login",
+			json={
 				"user": os.environ.get("EMAIL_TEST"),
 				"password": "somepass"
 			},
-		)
-		assert response.status_code == 200
+	)
+	assert response.status_code == 200
+
+def test_verify_token():
+	"""
+	Test verify Token
+	"""
+
+	response = client.post(
+	"/api/verify/token",
+	headers={"Authorization": f"Bearer {AuthTestHelper.get_token()}"},
+	json={
+			"user": os.environ.get("EMAIL_TEST"),
+			"password": "somepass"
+		},
+	)
+	assert response.status_code == 200
--- tests/test_user.py
+++ tests/test_user.py
@@ -20,86 +20,86 @@ client.base_url = os.environ.get("URL_LOCAL")
 
 
 class UserTestHelper:
+    """
+    User Helper tests
+    """
+
+    def get_user_by_email(email: str):
         """
-        User Helper tests
+        Get user by email test helper
         """
 
-        def get_user_by_email(email: str):
-            """
-			Get user by email test helper
-			"""
-
-            user = userObject(conn.local.user.find_one({"user": email}))
-            return jsonable_encoder(user)
+        user = userObject(conn.local.user.find_one({"user": email}))
+        return jsonable_encoder(user)
 
 def test_list_users():
-        """
-        Test list users
-        """
+    """
+    Test list users
+    """
 
-        token = AuthTestHelper.authenticate_user(os.environ.get("EMAIL_TEST"))
-        response = client.get(
-                    "/api/users",
-                    headers={"Authorization": f"Bearer {token}"})
-        assert response.status_code == 200
+    token = AuthTestHelper.authenticate_user(os.environ.get("EMAIL_TEST"))
+    response = client.get(
+                "/api/users",
+                headers={"Authorization": f"Bearer {token}"})
+    assert response.status_code == 200
 
 def test_retrieve_user():
-        """
-        Test retrieve user
-        """
+    """
+    Test retrieve user
+    """
 
-        user = UserTestHelper.get_user_by_email(os.environ.get("EMAIL_TEST"))
-        response = client.get(
-                    f"/api/users/{user['id']}",
-                    headers={"Authorization": f"Bearer {AuthTestHelper.get_token()}"})
-        assert response.status_code == 200
+    user = UserTestHelper.get_user_by_email(os.environ.get("EMAIL_TEST"))
+    response = client.get(
+                f"/api/users/{user['id']}",
+                headers={"Authorization": f"Bearer {AuthTestHelper.get_token()}"})
+    assert response.status_code == 200
 
 def test_create_user():
-        """
-        Test create user
-        """
+    """
+    Test create user
+    """
 
-        response = client.post(
-        "/api/users",
-        headers={"Authorization": f"Bearer {AuthTestHelper.get_token()}"},
-        json={
-            "user": fake.first_name()+"@"+fake.domain_name(),
-            "password": "somepass",
-            "name": fake.name(),
-            "surname": fake.text(max_nb_chars=5),
-            "second_surname": fake.text(max_nb_chars=5),
-            "college": fake.company()
-            },
-        )
-        assert response.status_code == 200
+    response = client.post(
+    "/api/users",
+    headers={"Authorization": f"Bearer {AuthTestHelper.get_token()}"},
+    json={
+        "user": fake.first_name()+"@"+fake.domain_name(),
+        "password": "somepass",
+        "name": fake.name(),
+        "surname": fake.text(max_nb_chars=5),
+        "second_surname": fake.text(max_nb_chars=5),
+        "college": fake.company()
+        },
+    )
+    assert response.status_code == 200
 
 def test_update_user():
-        """
-        Test update user
-        """
+    """
+    Test update user
+    """
 
-        user = UserTestHelper.get_user_by_email(os.environ.get("EMAIL_TEST"))
-        response = client.put(
-                    f"/api/users/{user['id']}",
-                    headers={"Authorization": f"Bearer {AuthTestHelper.get_token()}"},
-                    json={
-                        "user": os.environ.get("EMAIL_TEST"),
-                        "password": "somepass",
-                        "name": fake.name(),
-                        "surname": fake.text(max_nb_chars=5),
-                        "second_surname": fake.text(max_nb_chars=5),
-                        "college": fake.company()
-                        },
-                    )
-        assert response.status_code == 200
+    user = UserTestHelper.get_user_by_email(os.environ.get("EMAIL_TEST"))
+    response = client.put(
+                f"/api/users/{user['id']}",
+                headers={"Authorization": f"Bearer {AuthTestHelper.get_token()}"},
+                json={
+                    "user": os.environ.get("EMAIL_TEST"),
+                    "password": "somepass",
+                    "name": fake.name(),
+                    "surname": fake.text(max_nb_chars=5),
+                    "second_surname": fake.text(max_nb_chars=5),
+                    "college": fake.company()
+                    },
+                )
+    assert response.status_code == 200
 
 def test_delete_user():
-        """
-        Test delete user
-        """
+    """
+    Test delete user
+    """
 
-        user = UserTestHelper.get_user_by_email(os.environ.get("EMAIL_TEST"))
-        response = client.delete(
-        f"/api/users/{user['id']}",
-        headers={"Authorization": f"Bearer {AuthTestHelper.get_token()}"})
-        assert response.status_code == 204
+    user = UserTestHelper.get_user_by_email(os.environ.get("EMAIL_TEST"))
+    response = client.delete(
+    f"/api/users/{user['id']}",
+    headers={"Authorization": f"Bearer {AuthTestHelper.get_token()}"})
+    assert response.status_code == 204
